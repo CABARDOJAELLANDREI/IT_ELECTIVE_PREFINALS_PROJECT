1c481ce94f82166d8519145fe5719d797f9a3b0f
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 15:59:57 +0800

    feat: add HelpDeskContext and database configuration

--- ConsoleApp1/WebApplication1/Program.cs
+++ ConsoleApp1/WebApplication1/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<HelpDeskContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
