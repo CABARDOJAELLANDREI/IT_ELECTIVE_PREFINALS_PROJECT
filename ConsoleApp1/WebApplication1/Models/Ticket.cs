b2a0597495cde068251a0ae99b2e55c65b45260d
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 22:06:36 +0800

    added 2 models

---
+++ ConsoleApp1/WebApplication1/Models/Ticket.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDeskApp.Models;
+
+[Table("Tickets")]
+public class Ticket
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int CustomerId { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+
+    [Required]
+    public int PriorityId { get; set; }
+
+    [Required]
+    public int StatusId { get; set; }
+
+    [Required]
+    public string Subject { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+
+    [Required]
+    public string CreatedAt { get; set; } = string.Empty;
+
+    [Required]
+    public string UpdatedAt { get; set; } = string.Empty;
+
+    public string? DueAt { get; set; }
+    public string? ResolvedAt { get; set; }
+    public string? ClosedAt { get; set; }
+
+    [ForeignKey(nameof(CustomerId))]
+    public Customer Customer { get; set; } = null!;
+
+    [ForeignKey(nameof(CategoryId))]
+    public TicketCategory Category { get; set; } = null!;
+
+    [ForeignKey(nameof(PriorityId))]
+    public TicketPriority Priority { get; set; } = null!;
+
+    [ForeignKey(nameof(StatusId))]
+    public TicketStatus Status { get; set; } = null!;
+
+    public ICollection<TicketAssignment> TicketAssignments { get; set; } = new List<TicketAssignment>();
+    public ICollection<TicketComment> TicketComments { get; set; } = new List<TicketComment>();
+    public ICollection<TicketAttachment> TicketAttachments { get; set; } = new List<TicketAttachment>();
+    public ICollection<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+}
---
+++ ConsoleApp1/WebApplication1/Models/TicketAssignment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDeskApp.Models;
+
+[Table("TicketAssignments")]
+public class TicketAssignment
+{
+    [Required]
+    public int TicketId { get; set; }
+
+    [Required]
+    public int EmployeeId { get; set; }
+
+    [Required]
+    public string AssignedAt { get; set; } = string.Empty;
+
+    public string? UnassignedAt { get; set; }
+
+    public bool IsPrimary { get; set; } = false;
+
+    [ForeignKey(nameof(TicketId))]
+    public Ticket Ticket { get; set; } = null!;
+
+    [ForeignKey(nameof(EmployeeId))]
+    public Employee Employee { get; set; } = null!;
+}
