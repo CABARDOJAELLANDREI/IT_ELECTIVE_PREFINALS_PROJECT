570a73fd3a37051e47c7dc367d76e5b5c894a2c5
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 16:45:37 +0800

    feat: add Employee model file

--- ConsoleApp1/WebApplication1/Models/Employee.cs
+++ ConsoleApp1/WebApplication1/Models/Employee.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models
 {
-    public class Employee : Controller
+    [Table("Employees")]
+    public class Employee
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [ForeignKey("DepartmentId")]
+        public Department? Department { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        [ForeignKey("TeamId")]
+        public Team? Team { get; set; }
+
+        [Required]
+        public int IsActive { get; set; } = 1;
+
+        // Navigation properties
+        public ICollection<TeamMember> TeamMembers { get; set; } = new List<TeamMember>();
+        public ICollection<TicketAssignment> TicketAssignments { get; set; } = new List<TicketAssignment>();
+        public ICollection<TicketComment> TicketComments { get; set; } = new List<TicketComment>();
     }
 }
