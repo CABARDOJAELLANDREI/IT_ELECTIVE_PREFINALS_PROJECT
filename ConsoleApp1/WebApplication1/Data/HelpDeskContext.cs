2105f0893eb9ca226d3b3dfca3b22f4d162598ad
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 16:32:40 +0800

    feat: add Department model

--- ConsoleApp1/WebApplication1/Data/HelpDeskContext.cs
+++ ConsoleApp1/WebApplication1/Data/HelpDeskContext.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Reflection.Emit;
+using WebApplication1.Models;
 
-namespace WebApplication1.Data
+public class HelpDeskContext : DbContext
 {
-    public class HelpDeskContext : Controller
+    public HelpDeskContext(DbContextOptions<HelpDeskContext> options) : base(options) { }
+
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<TeamMember> TeamMembers { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<TicketCategory> TicketCategories { get; set; }
+    public DbSet<TicketPriority> TicketPriorities { get; set; }
+    public DbSet<TicketStatus> TicketStatuses { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<TicketAssignment> TicketAssignments { get; set; }
+    public DbSet<TicketComment> TicketComments { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<TicketTag> TicketTags { get; set; }
+    public DbSet<TicketAttachment> TicketAttachments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        base.OnModelCreating(modelBuilder);
+
+        // Composite keys for junction tables
+        modelBuilder.Entity<TeamMember>()
+            .HasKey(tm => new { tm.TeamId, tm.EmployeeId });
+
+        modelBuilder.Entity<TicketAssignment>()
+            .HasKey(ta => new { ta.TicketId, ta.EmployeeId });
+
+        modelBuilder.Entity<TicketTag>()
+            .HasKey(tt => new { tt.TicketId, tt.TagId });
+
+        modelBuilder.Entity<Team>()
+            .HasIndex(t => new { t.DepartmentId, t.Name })
+            .IsUnique();
     }
 }
---
+++ ConsoleApp1/WebApplication1/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Required]
+        public int IsActive { get; set; } = 1;
+
+        // Navigation properties
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        public ICollection<Team> Teams { get; set; } = new List<Team>();
+    }
+}
