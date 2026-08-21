bd4eef0a4132cdadbd41d1d985ceafcd3af31fd7
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 22:18:19 +0800

    addedd data

--- ConsoleApp1/WebApplication1/Data/HelpDeskDbContext.cs
+++ ConsoleApp1/WebApplication1/Data/HelpDeskDbContext.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelpDeskApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
-namespace WebApplication1.Data
+namespace HelpDeskApp.Data;
+
+public class HelpDeskDbContext : DbContext
 {
-    public class HelpDeskDbContext : Controller
+    public HelpDeskDbContext(DbContextOptions<HelpDeskDbContext> options) : base(options) { }
+
+    public DbSet<Department> Departments => Set<Department>();
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Team> Teams => Set<Team>();
+    public DbSet<TeamMember> TeamMembers => Set<TeamMember>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<TicketPriority> TicketPriorities => Set<TicketPriority>();
+    public DbSet<TicketStatus> TicketStatuses => Set<TicketStatus>();
+    public DbSet<TicketCategory> TicketCategories => Set<TicketCategory>();
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<TicketAssignment> TicketAssignments => Set<TicketAssignment>();
+    public DbSet<TicketComment> TicketComments => Set<TicketComment>();
+    public DbSet<TicketAttachment> TicketAttachments => Set<TicketAttachment>();
+    public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<TicketTag> TicketTags => Set<TicketTag>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        base.OnModelCreating(modelBuilder);
+
+        // Composite primary keys
+        modelBuilder.Entity<TeamMember>()
+            .HasKey(tm => new { tm.TeamId, tm.EmployeeId });
+
+        modelBuilder.Entity<TicketAssignment>()
+            .HasKey(ta => new { ta.TicketId, ta.EmployeeId });
+
+        modelBuilder.Entity<TicketTag>()
+            .HasKey(tt => new { tt.TicketId, tt.TagId });
+
+        // Self-referencing category relationship
+        modelBuilder.Entity<TicketCategory>()
+            .HasOne(c => c.ParentCategory)
+            .WithMany(c => c.SubCategories)
+            .HasForeignKey(c => c.ParentCategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
