f3e0aa9a28c32aee21c47e1108ea4010e75583e7
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 22:01:18 +0800

    a

---
+++ ConsoleApp1/WebApplication1/Models/TicketCategory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class TicketCategory : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
