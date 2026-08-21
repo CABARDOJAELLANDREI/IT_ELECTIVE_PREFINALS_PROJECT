a4d26746b488d2a63880d7a3fb9c814dab0e9f0e
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 21:57:57 +0800

    added controllers and models

---
+++ ConsoleApp1/WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskApp.Data;
+
+namespace HelpDeskApp.Controllers;
+
+public class CustomersController : Controller
+{
+    private readonly HelpDeskDbContext _context;
+
+    public CustomersController(HelpDeskDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var customers = await _context.Customers.AsNoTracking().ToListAsync();
+        return View(customers);
+    }
+}
---
+++ ConsoleApp1/WebApplication1/Controllers/TeamsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskApp.Data;
+
+namespace HelpDeskApp.Controllers;
+
+public class TeamsController : Controller
+{
+    private readonly HelpDeskDbContext _context;
+
+    public TeamsController(HelpDeskDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var teams = await _context.Teams
+            .Include(t => t.Department)
+            .Include(t => t.TeamMembers)
+                .ThenInclude(tm => tm.Employee)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return View(teams);
+    }
+}
