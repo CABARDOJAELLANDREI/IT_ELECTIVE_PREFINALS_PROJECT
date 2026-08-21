3eaffe05332e4f90cf980b8e2305968a2e4d9f93
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 22:17:08 +0800

    added data

---
+++ ConsoleApp1/WebApplication1/Data/HelpDeskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Data
+{
+    public class HelpDeskDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
