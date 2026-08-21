ced15f64fd418fbdf354550c527cc992006c7188
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 22:04:13 +0800

    added status and priority

---
+++ ConsoleApp1/WebApplication1/Models/TicketStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDeskApp.Models;
+
+[Table("TicketStatuses")]
+public class TicketStatus
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public bool IsClosed { get; set; } = false;
+
+    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
