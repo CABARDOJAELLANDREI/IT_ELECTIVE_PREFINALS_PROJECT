81b5983ba4186eb223c02dcf5027150d452d9e4f
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-21 21:15:13 +0800

    a

--- ConsoleApp1/WebApplication1/Controllers/DepartmentsController.cs
+++ ConsoleApp1/WebApplication1/Controllers/DepartmentsController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskApp.Data;
 
-namespace WebApplication1.Controllers
+namespace HelpDeskApp.Controllers;
+
+public class DepartmentsController : Controller
 {
-    public class DepartmentsController : Controller
+    private readonly HelpDeskDbContext _context;
+
+    public DepartmentsController(HelpDeskDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var departments = await _context.Departments
+            .Include(d => d.Employees)
+            .AsNoTracking()
+            .ToListAsync();
+        return View(departments);
     }
 }
